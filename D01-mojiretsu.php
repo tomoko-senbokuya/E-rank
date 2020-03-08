d02ee015b35b40a2dea40b4c219c9558207f100f
t833w2bma <t833w2@bma.biglobe.ne.jp>
2020-03-08 17:21:48 +0900

    answer

--- D01-mojiretsu.php
+++ D01-mojiretsu.php
@@ -1,9 +1,13 @@
 <?php
  $input = "Bootstrap". PHP_EOL;
  $int =  2 ;
- 
  // Botstrap  と出力しなさい
  
- $input = "water". PHP_EOL;
- $int = 5;
+// $input = "water". PHP_EOL;
+// $int = 5;
 // wate  上記と同じコードでwate と出力できるように
+  $str = str_split($input);
+  unset($str[$int]);
+  $res = rtrim( implode($str) );
+
+    echo $res;
